d1b279388d9ae7ea51fd81b1aadc727971469e96
ryancoder <yuanbwhust@gmail.com>
2013-06-02 01:35:03 +0800

    ok

--- nox/phidias/prelim.py
+++ nox/phidias/prelim.py
@@ -1,6 +1,19 @@
 # encoding:utf-8
 
 from pymongo import *
+import redis
+import json
+
+
+def flatten(iterable):
+    """Recursively iterate lists and tuples.
+    """
+    for elm in iterable:
+        if isinstance(elm, (list, tuple)):
+            for relm in flatten(elm):
+                yield relm
+        else:
+            yield elm
 
 
 class MongoUtil():
@@ -12,22 +25,50 @@ class MongoUtil():
     collection = db.movies
 
     def get_types(self, tag_name):
-        tags = self.collection.find({}, {tag_name: 1})
+        tags = collection.find({}, {tag_name: 1})
         tag_list = []
         for tag in tags:
-            if(tag[tag_name] != None):
-                tag_list.extend(tag[tag_name])
-        print tag_list
-        return tag_list
+            tag_list.append(tag.get(tag_name))
+        return list(flatten(tag_list))
 
-    def get_all_tags(self):
-        for name in self.tag_name_list[:1]:
-            self.get_types(name)
+
+class RedisUtil():
+    r = redis.StrictRedis(host='localhost')
+    tag_name_list = ['language', 'countries', 'tags', 'rate', 'people',
+                     'editors', 'directors', 'actors', 'year', 'date', 'length', 'types']
+
+    def get_types(self, unique_id, tag_name):
+        data = json.loads(self.r.get(unique_id))
+        feature_list = []
+        for item in data:
+            feature_list.append(item.get(tag_name))
+        return list(flatten(feature_list))
+
+    def get_split_feature(self, unique_id):
+        return self.r.members(unique_id+'feature')
+
+    def get_data(self, unique_id):
+        return json.loads(self.r.get(unique_id))
+
+    def set_data(self, unique_id, data):
+        self.r.set(unique_id, json.dumps(data))
+
+    def set_split_feature(self, unique_id, points):
+        self.r.sadd(unique_id+'feature', points)
+
+    def get_split_feature(self, unique_id):
+        self.r.get(unique_id+'feature')
+
+    def get_filter_points(self, unique_id):
+        return self.r.smembers(unique_id+'filter')
+
+    def append_filter_points(self, unique_id, points):
+        self.r.sadd(unique_id+'filter', points)
 
 
 def main():
     test = MongoUtil()
-    test.get_all_tags()
+    print test.get_types('types')
 
 # main
 if __name__ == "__main__":
