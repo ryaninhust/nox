b1fa09e14c6462adfaeb609b74cb3b570344467e
krazy <lixiangstar@gmail.com>
2013-06-02 01:12:44 +0800

    完善抓电影封面接口

--- nox/movie/views.py
+++ nox/movie/views.py
@@ -1,6 +1,9 @@
+# -*- coding: utf-8 -*-
 import os
+import json
 import urllib2
 import mimetypes
+import re
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.servers.basehttp import FileWrapper
@@ -20,17 +23,50 @@ def index_view(request):
     return render(request, "index.html")
 
 def photo_view(request, pid):
-    photo_url = "http://img3.douban.com/lpic/s" + pid + ".jpg"
-    filename = pid + ".jpg"
     cover_folder = os.path.abspath(os.path.join(settings.COVER_PATH, os.pardir,os.pardir, "cover/"))
     if not os.path.exists(cover_folder):
         os.mkdir(cover_folder)
 
+    filename = pid + ".jpg"
     dest_addr = os.path.abspath(os.path.join(settings.COVER_PATH, os.pardir,os.pardir, "cover/", filename))
     if os.path.exists(dest_addr):
         file_content = open(dest_addr).read()
     else:
-        stream = urllib2.urlopen(photo_url)
+        subject = "http://movie.douban.com/subject/" + pid + "/"
+        cv = re.compile(r"")
+        page = urllib2.urlopen(subject).read()
+        getLink = re.compile(r"img.*?(?<=src=\")(.*?)(?=\"\s*title=\"点击)")
+        getLink2 = re.compile(r"a.*?(?<=href=\")(.*?)(?=\"\s*title=\"点击)")
+        img_results = getLink.findall(page)
+        if img_results:
+            img_name = img_results[0]
+        else:
+            img_results = getLink2.findall(page)
+            if img_results:
+                img_name = img_results[0]
+            else:
+                img_name = "s1291545.jpg"
+
+        try:
+            img_name.index("pic")
+            photo_url = "http://img3.douban.com/lpic/" + img_name.split("/")[-1]
+        except:
+            get_img = re.compile(r"\w(\d+)\.jpg")
+            if get_img.findall(img_name):
+                img_id = get_img.findall(img_name)[0]
+            else:
+                img_id = "1291545"
+            photo_url = "http://img3.douban.com/view/photo/photo/public/p"+ img_id + ".jpg"
+
+        print photo_url
+        try:
+            stream = urllib2.urlopen(photo_url)
+        except:
+            try:
+                another_url =  "http://img3.douban.com/lpic/s" + pid + ".jpg"
+                stream = urllib2.urlopen(another_url)
+            except:
+                pass
         file_content = stream.read()
         new_file = open(dest_addr, "wb")
         new_file.write(file_content)
@@ -40,8 +76,8 @@ def photo_view(request, pid):
 
 
 class MovieViewSet(viewsets.ViewSetMixin,
-                   generics.GenericAPIView,
-                   mixins.SubModelMixin):
+        generics.GenericAPIView,
+        mixins.SubModelMixin):
 
     serializer_class = MovieSerializer
     def get_movies(self, request, *args, **kwargs):
@@ -57,13 +93,13 @@ class MovieViewSet(viewsets.ViewSetMixin,
                       watcher='b', tags=["b"])
         """
         movie = Movie(id="3642843", name="a", directors="a", actors="a",
-                      types="a", countries="a", languages='a',
-                      year='a', length='a', rate='a',
-                      watcher='a', tags="a", cover_url="/photos/3642843")
+                types="a", countries="a", languages='a',
+                year='a', length='a', rate='a',
+                watcher='a', tags="a", cover_url="/photos/3642843")
         bmovie = Movie(id="11529526", name="b", directors="b", actors="b",
-                      types="b", countries="b", languages="b",
-                      year='b', length='b', rate='b',
-                      watcher='b', tags="b", cover_url="/photos/11529526")
+                types="b", countries="b", languages="b",
+                year='b', length='b', rate='b',
+                watcher='b', tags="b", cover_url="/photos/11529526")
         movies.append(movie)
         movies.append(bmovie)
         movie_json = MovieSerializer(movies)
@@ -71,7 +107,7 @@ class MovieViewSet(viewsets.ViewSetMixin,
 
 
 class QuestionViewSet(viewsets.ViewSetMixin,
-                      generics.GenericAPIView):
+        generics.GenericAPIView):
 
     serializer_class = QuestionSerializer
     def answer_question(self, request, *args, **kwargs):
